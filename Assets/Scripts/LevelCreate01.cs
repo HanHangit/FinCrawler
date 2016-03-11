a8a953d9b9d893036b3d195f2d384895d99b7709
HanHangit <clonkcool@yahoo.de>
2016-03-11 10:00:46 +0100

    Wall-Stone Tiled Prefab

---
+++ Assets/Scripts/LevelCreate01.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelCreate01 : MonoBehaviour {
+    public GameObject[] Wall;
+    public GameObject[] Background;
+    public int MaxHeight;
+    public int MaxWidth;
+
+
+	// Use this for initialization
+	void Start () {
+        	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
