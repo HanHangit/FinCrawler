18922502e91a7864967c04036ecaf84de28b8d77
wookiepeter <matthis.hagen@o2online.de>
2016-03-11 17:05:46 +0100

    edited enemy01;

---
+++ Assets/Scripts/EnemyHealthController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealthController : MonoBehaviour {
+
+    public float startHealth;
+    float health;
+
+    void Start()
+    {
+        health = startHealth;
+    }
+
+	void ApplyDamage(float damage)
+    {
+        health -= damage;
+        if(health <= 0)
+        {
+            Dying();
+        }
+    }
+
+    void Dying()
+    {
+        Destroy(gameObject);
+    }
+}
